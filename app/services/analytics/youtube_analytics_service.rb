3d6437b307a9b65871c80ee64d7f48636d2a9657
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2020-08-14 15:00:07 +0800

    Zoom done

--- app/services/analytics/youtube_analytics_service.rb
+++ app/services/analytics/youtube_analytics_service.rb
@@ -1,7 +1,5 @@
 class Analytics::YoutubeAnalyticsService
   class << self
     # Service account does not work
-    # If Sushivid Youtube account is using @sushivid.com domain (using G Suite Domain)
-    # Can get access token on behalf @sushivid
   end
 end
--- app/services/analytics/zoom_service.rb
+++ app/services/analytics/zoom_service.rb
@@ -1,18 +1,36 @@
 class Analytics::ZoomService
   class << self
+    def get_webinar_full_stats
+      webinars = get_webinar_lists[:webinars]&.filter{|web| DateTime.parse(web[:start_time]) < (DateTime.now - 1.day)}
+      webinars.map do |web|
+        stats = get_webinar_stats(web[:id])
+        {
+          stats: stats[:topic],
+          start_time: date_convert(stats[:start_time]),
+          end_time: date_convert(stats[:end_time]),
+          total_minutes: stats[:total_minutes],
+          participants: stats[:participants_count]
+        }
+      end
+    end
+
+    private
+    def date_convert(datetime = nil)
+      (DateTime.parse(datetime) + 8.hours).strftime('%d %B %Y - %H%M HRS') if datetime
+    end
+
     def get_webinar_lists
       url = "https://api.zoom.us/v2/users/#{Rails.application.credentials.zoom[:user_id]}/webinars"
       response = call(url)
       parse_response(response)
     end
 
-    def get_webinar_stats
-      url = "https://api.zoom.us/v2/report/webinars/#{params[:webinar_id]}"
+    def get_webinar_stats(webinar_id)
+      url = "https://api.zoom.us/v2/report/webinars/#{webinar_id}"
       response = call(url)
       parse_response(response)
     end
 
-    private
     def call(url)
       Faraday.get(url) do |req|
         req.headers['Authorization'] = "Bearer #{encode_payload}"
